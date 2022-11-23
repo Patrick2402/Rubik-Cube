0a8d6da9af8e53550abd27773944e2996ceb2190
Patryk <92256790+Patrick2402@users.noreply.github.com>
2022-11-23 13:00:53 +0100

    Update Homapage.js

--- src/pages/Homapage.js
+++ src/pages/Homapage.js
@@ -1,7 +1,12 @@
+import {Link,Outlet} from 'react-router-dom'
+
 const Homepage = () => {
     return (  
         <>
-            <h1>HOMEPAGE1§</h1>
+    <Link className="brick"to="/">Home</Link>
+        <Link className="brick"to="/timer">Timer</Link>
+        <Link className="brick"to="/registration">Registration</Link>
+        <Outlet />
         </>
         );
 }
